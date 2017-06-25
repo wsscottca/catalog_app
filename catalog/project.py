3fbfa599479f85b081b8c9cfa3c41a980b04c801
Will Scott <WSScottCA@gmail.com>
2017-06-24 17:27:30 -0700

    Update project.py

--- catalog/project.py
+++ catalog/project.py
@@ -23,7 +23,7 @@ CLIENT_ID = json.loads(
 APPLICATION_NAME = "Catalog App"
 
 # Connect to Database and create database session
-engine = create_engine('sqlite:///catalog.db')
+engine = create_engine('postgresql://catalog:password@localhost/catalog')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
